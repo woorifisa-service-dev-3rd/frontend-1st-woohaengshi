39f662d1fdc7d8ac4c7b6237e858a26de0908ae3
Hyunwoo Kang <gusdn741021@gmail.com>
2024-07-18 08:37:14 +0900

    factort: 파일 이름 변경 및 키 추가:recycle:

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,16 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
 import { getFirestore, collection, doc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-}
+    apiKey: "AIzaSyCf9ttqBTXBf_e4AHASQaK6-LlQ4FQJcuM",
+    authDomain: "test-94c7d.firebaseapp.com",
+    projectId: "test-94c7d",
+    storageBucket: "test-94c7d.appspot.com",
+    messagingSenderId: "169749339570",
+    appId: "1:169749339570:web:d2faf9a789e06a442758ab"
+};
+
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
