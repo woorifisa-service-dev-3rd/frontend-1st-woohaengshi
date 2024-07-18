1ce31e4766eaa10e8874d0db0c848c81bf4869bc
Hyunwoo Kang <gusdn741021@gmail.com>
2024-07-18 11:36:02 +0900

    [feat/#32]: 회원가입 시 입력 확인 체크 기능 추가 :sparkles:

--- js/join.js
+++ js/join.js
@@ -18,45 +18,52 @@ function handleRadioButtonChange() {
 }
 
 
-buttonDom.addEventListener('click', (event)=>{
+buttonDom.addEventListener('click', (event) => {
     event.preventDefault();
-    let isJoinSuccess = false; 
+    let isJoinSuccess = false;
+
+    // 사용자 이름과 비밀번호 검증
+    if (userName.value.trim() === '' || password.value.trim() === '') {
+        alert('사용자 이름과 비밀번호를 입력해주세요.');
+        return;
+    }
 
     // 사용자 정보 가져오기
     db.collection("user")
-    .get()
-    .then((result) => {
-        // console.log("Documents count:", result.size);
-        let isDuplicateUser = false;
-        result.docs.forEach((doc) => {
-            // console.log("Document data:", doc.data());
-            if(userName.value === doc.data().name) {
-                isDuplicateUser = true;
-                return;
-            }
-        });
-
-        if(!isDuplicateUser) {
-            // 새로운 사용자 등록
-            db.collection("user").add({
-                name: userName.value,
-                password: password.value,
-                class: selectedLabel
-            })
-            .then(() => {
-                alert('회원가입 성공!');
-                isJoinSuccess = true;
-                window.location.href = 'login.html';
-            })
-            .catch((error) => {
-                alert('회원가입 실패: ' + error.message);
+        .get()
+        .then((result) => {
+            // console.log("Documents count:", result.size);
+            let isDuplicateUser = false;
+            result.docs.forEach((doc) => {
+                // console.log("Document data:", doc.data());
+                if (userName.value === doc.data().name) {
+                    isDuplicateUser = true;
+                    return;
+                }
             });
-        } else {
-            alert('이미 존재하는 사용자 이름입니다.');
-        }
 
-        // if(!isJoinSuccess) { // 회원가입이 실패한 경우
-        //     alert('회원가입 실패');
-        // }
-    });
+
+            if (!isDuplicateUser) {
+                // 새로운 사용자 등록
+                db.collection("user").add({
+                    name: userName.value,
+                    password: password.value,
+                    class: selectedLabel
+                })
+                .then(() => {
+                    alert('회원가입 성공!');
+                    isJoinSuccess = true;
+                    window.location.href = 'login.html';
+                })
+                .catch((error) => {
+                    alert('회원가입 실패: ' + error.message);
+                });
+            } else {
+                alert('이미 존재하는 사용자 이름입니다.');
+            }
+
+            // if (!isJoinSuccess) {
+            //     alert('회원가입 실패');
+            // }
+        });
 });
