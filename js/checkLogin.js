38103b72bb4eff6b16b804224db2e21a5db6c37e
Hyunwoo Kang <gusdn741021@gmail.com>
2024-07-18 14:06:19 +0900

    [feat/#32]: 로그아웃 추가 :sparkles:

--- js/checkLogin.js
+++ js/checkLogin.js
@@ -2,3 +2,10 @@ if(window.localStorage.getItem('userId') === null){
     window.location.href = 'login.html';
     console.log(window.localStorage.getItem('userId'));
 }
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      localStorage.clear();
+      window.location.href = 'login.html';
+    }
+});
